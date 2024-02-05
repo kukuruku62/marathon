eb579e13b5245b6b67ae0a3302d89110de212a1d
Aleksei Falchuk <sddw28@gmail.com>
2024-02-05 22:14:19 +0100

    refactor: change structure of the schedule component

--- marathon/src/components/Schedule/Schedule.jsx
+++ marathon/src/components/Schedule/Schedule.jsx
@@ -3,43 +3,38 @@ import styles from "./Schedule.module.scss";
 import { Skeleton } from "../Skeleton/Skeleton";
 import { Link } from "react-router-dom";
 
-export const Schedule = ({ statusOfFetch, listOfEvents, errorMessage }) => {
+export const Schedule = ({ statusOfFetch, listEvents, errorMessage }) => {
   return (
-    <section className={styles.schedule}>
-      <h2 className={styles.title}>Schedule</h2>
+    <section>
+      <h2 className={styles.title}>ROZVRH</h2>
       {statusOfFetch === "loading" && <Skeleton message={"Loading..."} />}
       {statusOfFetch === "rejected" && errorMessage ? (
         <Skeleton message={"Error:( Please, try again."} />
       ) : null}
-      {statusOfFetch === "resolved" && !listOfEvents.length && (
+      {statusOfFetch === "resolved" && !listEvents.length && (
         <Skeleton message={"There is no any events yet"} />
       )}
-      {statusOfFetch === "resolved" && listOfEvents.length ? (
-        <ol className={styles.grid}>
-          {listOfEvents.map((event, index) => (
-            // TODO: change key to event.id
-            <li key={index} className={styles.gridItem}>
-              <Link to={`/event/${event.id}`}>
-                <article className={styles.content}>
-                  <div className={styles.subtitlesWrapper}>
-                    <h3 className={styles.subtitle}>{event.place}</h3>
-                    <h3 className={styles.subtitle}>{event.variant}</h3>
-                    <h3 className={styles.subtitle}>{event.type}</h3>
-                  </div>
-                  <div className={styles.distanceContainer}>
-                    {event.length.map((el, index) => (
-                      <p key={index} className={styles.distance}>
-                        {el}
-                      </p>
-                    ))}
-                    {<p className={styles.measurement}>{event.measurement}</p>}
-                  </div>
-                  <p className={styles.date}>{event.date.split("-").reverse().join(".")}</p>
-                </article>
-              </Link>
-            </li>
-          ))}
-        </ol>
+      {statusOfFetch === "resolved" && listEvents.length ? (
+        <ul className={styles.wrapper}>
+          {listEvents.map(({ _id, name, dateOfEvent, timeOfStartEvent, lengthsOfRoutes, measurement }) => (
+              <li key={_id} className={styles.gridItem}>
+                <Link to={`/event/${_id}`}>
+                  <article className={styles.content}>
+                      <h3 className={styles.subtitle}>{name}</h3>
+                    <div className={styles.distanceContainer}>
+                      {lengthsOfRoutes.map((length, index) => (<p key={index} className={styles.distance}>{length}</p>))}
+                      {<p className={styles.measurement}>{measurement}</p>}
+                    </div>
+                    <div className={styles.dateAndTimeWrapper}>
+                      <p className={styles.date}>{dateOfEvent.split("-").reverse().join(".")}</p>
+                      <p className={styles.date}>{timeOfStartEvent}</p>
+                    </div>
+                  </article>
+                </Link>
+              </li>
+            )
+          )}
+        </ul>
       ) : null}
     </section>
   );
