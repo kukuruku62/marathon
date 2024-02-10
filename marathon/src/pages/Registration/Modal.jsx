7e258e80ce26f37472d1da35c0056258f54c8983
Aleksei Falchuk <sddw28@gmail.com>
2024-02-10 13:08:01 +0100

    refactor: change name of the prop modal component

--- marathon/src/pages/Registration/Modal.jsx
+++ marathon/src/pages/Registration/Modal.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import styles from "./Registration.module.scss";
 
-export const Modal = ({ toggleModal, statusResponse }) => {
+export const Modal = ({ toggleModal, messageResponse }) => {
 
   return (
     <div className={styles.overlay} onClick={toggleModal}>
       <div className={styles.modalContent}>
-        <p>{statusResponse}</p>
+        <p>{messageResponse}</p>
         <button className={styles.btnClose} onClick={toggleModal}>
           Close
         </button>
