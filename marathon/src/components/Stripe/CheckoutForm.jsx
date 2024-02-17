4981fce279a895661a7550baab8bc3a79a8ce662
Aleksei Falchuk <sddw28@gmail.com>
2024-02-17 12:04:28 +0100

    feat: add stripe frontend component with redirect page

---
+++ marathon/src/components/Stripe/CheckoutForm.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+import { EmbeddedCheckoutProvider, EmbeddedCheckout} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+
+const stripePromise = loadStripe("pk_test_51OdBLyChoV7wOHwcTxOxVlZnBTreYosfcuhsdgCF5O5qPN7IM1PX4qENPRV35xA2ide5ld7q29q404MZuBQzjjzU00SNON6zN9");
+
+export const CheckoutForm = () => {
+  const participant = useSelector((state) => state.events.registeredParticipant)
+  const [clientSecret, setClientSecret] = useState('');
+
+  useEffect(() => {
+    fetch("http://localhost:3002/api/stripe/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(participant)
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret)); 
+  }, []);
+
+  return (
+      <div id="checkout" style={{padding: "30px 0"}} >
+        {clientSecret && (
+          <EmbeddedCheckoutProvider 
+            stripe={stripePromise}
+            options={{clientSecret}}
+          >
+            <EmbeddedCheckout />
+          </EmbeddedCheckoutProvider>
+        )}
+      </div>
+  )
+}
---
+++ marathon/src/components/Stripe/Return.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { SuccessPayment } from "./SuccessPayment";
+
+export const Return = () => {
+  const [status, setStatus] = useState(null);
+  const [customerEmail, setCustomerEmail] = useState("");
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const sessionId = urlParams.get("session_id");
+
+    fetch(`http://localhost:3002/api/stripe/session-status?session_id=${sessionId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setStatus(data.status);
+        setCustomerEmail(data.customer_email);
+      });
+  }, []);
+
+  if (status === "open") {
+    return <Navigate to="/checkout" />;
+  }
+
+  if (status === "complete") {
+    return (
+      <SuccessPayment customerEmail={customerEmail} />
+    );
+  }
+
+  return null;
+};
