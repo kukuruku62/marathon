cba2a115338c44a83c408e2d19b5cc2d8300459f
Aleksei Falchuk <sddw28@gmail.com>
2024-02-18 21:50:40 +0100

    feat: add optimistic loading to participants component

--- marathon/src/pages/Participants/Participants.jsx
+++ marathon/src/pages/Participants/Participants.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import { SkeletonBike } from "../../components/SkeletonBike/SkeletonBike";
 import { useSingleEvent } from "../../hooks/useSingleEvent";
@@ -7,6 +8,7 @@ import styles from "./Participants.module.scss";
 import { useParams } from "react-router-dom";
 
 export const Participants = () => {
+  const status = useSelector((state) => state.events.status);
   const { id } = useParams();
   const { name, participants } = useSingleEvent(id);
 
@@ -14,13 +16,13 @@ export const Participants = () => {
   return (
     <section className={styles.wrapper}>
       <div className={styles.inner}>
+        {status === "loading" && <SkeletonBike />}
         {name && <h2 className={styles.title}>{name}</h2>}
         <h3 className={styles.subtitle}>Registrovaní účastníci:</h3>
-        {/* {statusFetch === "loading" && <SkeletonBike />} */}
         {participants && (
           <ul className={styles.list}>
             {participants.map(({ _id, firstName, lastName, distance }, index) => (
-              <li className={styles.itemList} key={_id}>{`${ index + 1 + "." } ${firstName} ${lastName}, distance: ${distance}`}</li>
+              <li className={styles.itemList} key={_id}>{`${index + 1 + "."} ${firstName} ${lastName}, distance: ${distance}`}</li>
             ))}
           </ul>
         )}
