93e78569263eb9e50510d76798a40c7a75a2ed43
Aleksei Falchuk <sddw28@gmail.com>
2024-02-19 10:25:02 +0100

    feat: add optimistic loading to the stripe return component + fetch catch error

--- marathon/src/components/Stripe/Return.jsx
+++ marathon/src/components/Stripe/Return.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { SuccessPayment } from "./SuccessPayment";
 
+import { SkeletonBike } from "../SkeletonBike/SkeletonBike";
+
 export const Return = () => {
   const [status, setStatus] = useState(null);
+  const [error, setError] = useState(false);
   const [customerEmail, setCustomerEmail] = useState("");
 
   useEffect(() => {
@@ -16,17 +19,23 @@ export const Return = () => {
       .then((data) => {
         setStatus(data.status);
         setCustomerEmail(data.customer_email);
+      })
+      .catch((error) => {
+        setError(true);
       });
   }, []);
 
+  if (status === null) {
+    return <SkeletonBike />;
+  }
+
+  //TODO: CHECK REDIRECT AFTER FAILED PAYMENT
   if (status === "open") {
     return <Navigate to="/checkout" />;
   }
 
   if (status === "complete") {
-    return (
-      <SuccessPayment customerEmail={customerEmail} />
-    );
+    return <SuccessPayment customerEmail={customerEmail} />;
   }
 
   return null;
