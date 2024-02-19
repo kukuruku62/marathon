b83dfbb49cc1398f7ff65e48117c2267faff6d39
Aleksei Falchuk <sddw28@gmail.com>
2024-02-19 10:55:11 +0100

    refactor: add few routes to app

--- marathon/src/App.jsx
+++ marathon/src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 // import {} from "react-error-boundary" // УДАЛИТЬ ЭРОР БОУНДАРИ, ЕСЛИ НЕ НУЖЕН
 import {
   createBrowserRouter,
@@ -12,13 +12,21 @@ import { Layout } from "./components/Layout/Layout.jsx";
 import { ErrorPage } from "./pages/ErrorPage/ErrorPage.jsx";
 import { SingleEvent } from "./pages/SingleEvent/SingleEvent.jsx";
 import { Registration } from "./pages/Registration/Registration.jsx";
+import { Participants } from "./pages/Participants/Participants.jsx";
+import { CheckoutForm } from "./components/Stripe/CheckoutForm.jsx";
+import { Return } from "./components/Stripe/Return.jsx";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
       <Route index element={<Home />} />
-      <Route path="event/:id" element={<SingleEvent />} />
-      <Route path="registration" element={<Registration />} />
+      <Route path="events/:id" element={<SingleEvent />} />
+      <Route path="events/:id/registration" element={<Registration />} />
+      <Route path="events/:id/participants" element={<Participants />} />
+
+      <Route path="payment/checkout" element={<CheckoutForm />} />
+      <Route path="payment/return" element={<Return />} />
     </Route>
   )
 );
