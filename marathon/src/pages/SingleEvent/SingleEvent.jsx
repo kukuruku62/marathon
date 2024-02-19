a8c01ab8983da1116298e9a5596d238e2464fea4
Aleksei Falchuk <sddw28@gmail.com>
2024-02-19 12:03:30 +0100

    refactor: change structure of the singleevent component

--- marathon/src/pages/SingleEvent/SingleEvent.jsx
+++ marathon/src/pages/SingleEvent/SingleEvent.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 import { useParams, Link } from "react-router-dom";
 import styles from "./SingleEvent.module.scss";
@@ -13,9 +12,9 @@ import { SkeletonBike } from "../../components/SkeletonBike/SkeletonBike.jsx";
 
 
 export const SingleEvent = () => {
-  const status = useSelector((state) => state.events.status);
   const { id } = useParams();
-  const { 
+  const {
+    status,
     _id,
     name,
     place,
@@ -25,7 +24,6 @@ export const SingleEvent = () => {
     timeOfStartEvent,
     distances,
     measurement,
-    startpoint,
     registration,
     addRegistration,
     presentation,
@@ -44,33 +42,33 @@ export const SingleEvent = () => {
     <section className={styles.wrapper}>
       {status === "loading" && <SkeletonBike />}
       {status === "resolved" &&
-      <>
-        <div className={styles.content}>
-          <h2 className={styles.title}>{name}</h2>
-          <Link to={`/events/${_id}/registration`} className={styles.linkRegistration} target="_blank"> Registrácia </Link>
+        <>
+          <div className={styles.content}>
+            <h2 className={styles.title}>{name}</h2>
+            <Link to={`/events/${_id}/registration`} className={styles.linkRegistration} target="_blank"> Registrácia </Link>
 
-          {sponsorsEvent && <Sponsors sponsorsEvent={sponsorsEvent} />}
+            {sponsorsEvent && <Sponsors sponsorsEvent={sponsorsEvent} />}
 
-          <Link to={`/events/${_id}/participants`} className={styles.linkParticipants} target="_blank">
-            <h3>Zoznam prihlásených účastníkov</h3>
-          </Link>
+            <Link to={`/events/${_id}/participants`} className={styles.linkParticipants} target="_blank">
+              <h3>Zoznam prihlásených účastníkov</h3>
+            </Link>
 
-          <ul className={styles.listItems}>
-            <Item title="Miesto:" prop={place} />
-            <Item title="Deň:" prop={dateOfEvent} additionalInfo={addDate}/>
-            <Item title="Čas:" prop={timeOfStartEvent} additionalInfo={addTimeStartEvent}/>
-            <Item title="Dĺžka trasy:" propArr={distances} measurement={measurement} />
-            <Categories title="Kategórie:" 
-                                      categoriesMale={categoriesMale} 
-                                      categoriesFemale={categoriesFemale} 
-                                      additionalInfo={addCategories}/>
-            <Item title="Cena:" propArr={payments} additionalInfo={addPayments}/>
-            <Item title="Registrácia:" propArr={registration} additionalInfo={addRegistration}/>
-            <Item title="Prezentácia:" propArr={presentation} additionalInfo={addPresentation}/>
-          </ul>
-        </div>
-        <MapOfEvent />
-      </>
+            <ul className={styles.listItems}>
+              <Item title="Miesto:" prop={place} />
+              <Item title="Deň:" prop={dateOfEvent} additionalInfo={addDate}/>
+              <Item title="Čas:" prop={timeOfStartEvent} additionalInfo={addTimeStartEvent}/>
+              <Item title="Dĺžka trasy:" propArr={distances} measurement={measurement} />
+              <Categories title="Kategórie:" 
+                                        categoriesMale={categoriesMale} 
+                                        categoriesFemale={categoriesFemale} 
+                                        additionalInfo={addCategories}/>
+              <Item title="Cena:" propArr={payments} additionalInfo={addPayments}/>
+              <Item title="Registrácia:" propArr={registration} additionalInfo={addRegistration}/>
+              <Item title="Prezentácia:" propArr={presentation} additionalInfo={addPresentation}/>
+            </ul>
+          </div>
+          <MapOfEvent />
+        </>
       }
     </section>
   );
