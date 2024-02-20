740f64c7a4df809aa13d6f86818d2e524155cb35
Aleksei Falchuk <sddw28@gmail.com>
2024-02-20 21:00:08 +0100

    refactor: deny last changes

--- marathon/src/components/MapOfEvent/MapOfEvent.jsx
+++ marathon/src/components/MapOfEvent/MapOfEvent.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 
-
-import "leaflet/dist/leaflet.css";
-import L from 'leaflet';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-    iconUrl: markerIcon.src,
-    iconRetinaUrl: markerIcon2x.src,
-    shadowUrl: markerShadow.src,
-})
 import { MapContainer, TileLayer, Marker, Tooltip, Polyline } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/images/marker-icon-2x.png";
 
 import styles from "./MapOfEvent.module.scss";
 
