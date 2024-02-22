4a1dd938e053f5cd4bbd5bff55337e0da3e6e99f
Aleksei Falchuk <sddw28@gmail.com>
2024-02-22 12:46:42 +0100

    refactor: change props to useselector in about component

--- marathon/src/components/About/About.jsx
+++ marathon/src/components/About/About.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import styles from "./About.module.scss";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-export const About = ({dateOfNextEvent, firstEvent}) => {
+export const About = () => {
+  const firstEvent = useSelector((state) => state.events.firstEvent);
+  const dateOfNextEvent = useSelector((state) => state.events.dateOfNextEvent);
+
   return (
     <section className={styles.slider}>
       <div className={styles.wrapper}>
         <div className={styles.textContainer}>
-        {firstEvent && <h1 className={styles.title}>{`${firstEvent.name} ${dateOfNextEvent} ${firstEvent.timeOfStartEvent} `} </h1>}
+          {firstEvent && dateOfNextEvent && (
+            <h1 className={styles.title}>
+              {`${firstEvent.name} ${dateOfNextEvent} ${firstEvent.timeOfStartEvent} `}
+            </h1>
+          )}
           <p className={styles.description}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sequi, expedita neque
             quod consequatur minima voluptas impedit obcaecati veritatis, reprehenderit dolorem
@@ -15,27 +24,20 @@ export const About = ({dateOfNextEvent, firstEvent}) => {
         </div>
         <div className={styles.linksBlock}>
           <nav>
-            <ul className={styles.linksBlock__container}>
-              <li className={styles.linksBlock__link_wrapper}>
-                <a className={styles.linksBlock__link} href="">
-                  Зарегистрироваться
-                </a>
+            <ul>
+              <li className={styles.linkWrapper}>
+                <Link className={styles.link}>Registrácia</Link>
               </li>
-              {/* <li className={styles.linksBlock__link_wrapper}>
-                <a className={styles.linksBlock__link} href="">
-                  Посмотреть расписание
-                </a>
-              </li> */}
-              <li className={styles.linksBlock__link_wrapper}>
-                <a className={styles.linksBlock__link} href="">
-                  Поддержать проект
-                </a>
+              <li className={styles.linkWrapper}>
+                <Link className={styles.link}>Spojte sa s nami</Link>
+              </li>
+              <li className={styles.linkWrapper}>
+                <Link className={styles.link}>Podpor nás</Link>
               </li>
             </ul>
           </nav>
         </div>
       </div>
-      {/* <img className={styles.sliderImg} src="../src/assets/img/start-marathon.jpg" alt="Image start of the marathon" /> */}
     </section>
   );
 };
