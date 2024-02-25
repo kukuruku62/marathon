2850ed1ebfacfb1089ee972e1ac1af0a9789938f
Aleksei Falchuk <sddw28@gmail.com>
2024-02-25 20:12:02 +0100

    refactor: update footer css and add main container for app

--- marathon/src/main.jsx
+++ marathon/src/main.jsx
@@ -9,6 +9,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <div className="container" >
     <App />
+    </div>
   </Provider>
 );
