b3ea58d5549294a888c9cecc70c5f2c7bf81ca49
Aleksei Falchuk <sddw28@gmail.com>
2024-02-25 16:17:01 +0100

    feat: update the participants component in case of zero quantity

--- marathon/src/pages/Participants/Participants.jsx
+++ marathon/src/pages/Participants/Participants.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 import { SkeletonBike } from "../../components/SkeletonBike/SkeletonBike";
+import SmileIcon from "../../assets/svg/smile.svg?react";
 import { useSingleEvent } from "../../hooks/useSingleEvent";
 
 import styles from "./Participants.module.scss";
@@ -19,12 +20,22 @@ export const Participants = () => {
         {status === "resolved" && 
           <>
             <h2 className={styles.title}>{name}</h2>
-            <h3 className={styles.subtitle}>Registrovaní účastníci:</h3>
-            <ul className={styles.list}>
-              {participants.map(({ _id, firstName, lastName, distance }, index) => (
-                <li className={styles.itemList} key={_id}>{`${index + 1 + "."} ${firstName} ${lastName}, distance: ${distance}`}</li>
-              ))}
-            </ul>
+            {participants.length === 0 && 
+              <div className={styles.noParticipants}>
+                <h3 className={styles.subtitle}>Môžete byť prvým účastníkom</h3>
+                <SmileIcon className={styles.smile}/>
+              </div>
+            }
+            {participants.length > 0 && 
+              <>
+                <h3 className={styles.subtitle}>Registrovaní účastníci:</h3>
+                <ul className={styles.list}>
+                  {participants.map(({ _id, firstName, lastName, distance }, index) => (
+                    <li className={styles.itemList} key={_id}>{`${index + 1 + "."} ${firstName} ${lastName}, distance: ${distance}`}</li>
+                  ))}
+                </ul>
+              </>
+            }
           </>
         }
       </div>
