05dc5001d50c2ba94a8770f8519043ee588482bf
Aleksei Falchuk <sddw28@gmail.com>
2024-02-27 13:58:47 +0100

    refactor: update advertising component

--- marathon/src/components/Advertising/Advertising.jsx
+++ marathon/src/components/Advertising/Advertising.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchImagesMainSponsors } from "../../redux/mainSponsorsSlice.js";
 import styles from "./Advertising.module.scss";
 
+export const Advertising = () => {
+  const dispatch = useDispatch();
+  const fetchStatus = useSelector((state) => state.imagesSponsors.fetchStatus);
+  const arrImages = useSelector((state) => state.imagesSponsors.arrImages);
 
-export const Advertising = ({listMainSponsors}) => {
+  useEffect(() => {
+    dispatch(fetchImagesMainSponsors());
+  }, [dispatch]);
 
   return (
-    <section>
-      <div className={styles.wrapperSponsors}>
-          <h2 className={styles.titleAdv}>Sponsors and partners:</h2>
-          <ul className={styles.advertising}>
-            {listMainSponsors &&
-              listMainSponsors.map(({ linkSite, linkImage }, index) => (
-                <li key={index}>
-                  <a href={linkSite} target="_blank">
-                    <img className={styles.sponsorImg} src={linkImage} />
-                  </a>
+    <>
+      {fetchStatus === "resolved" && arrImages.length > 0 && (
+        <section className={styles.wrapper}>
+          <div className={styles.inner}>
+            <h2 className={styles.title}>Sponsors and partners:</h2>
+             <ul className={styles.advertising}>
+              { arrImages.map(({_id, linkImage, linkSite}) => 
+                <li key={_id}>
+                  <Link to={linkSite} target="_blank" rel="noopener noreferrer">
+                    <img className={styles.sponsorImg} src={linkImage} alt="Picture of the sponsor" />
+                  </Link>
                 </li>
-              ))}
-          </ul>
-        </div>
-    </section>
+              )}
+            </ul>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
--- marathon/src/redux/store.js
+++ marathon/src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import eventsSliceReducer from "./listEventsSlice";
+import imagesMainSponsorsSlice from "./mainSponsorsSlice"
+
 
 export const store = configureStore({
   reducer: {
     events: eventsSliceReducer,
+    imagesSponsors: imagesMainSponsorsSlice,
   },
 });
